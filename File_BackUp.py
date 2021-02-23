5a4310952193e15e253a1fd89cde780aa1458378
Federico Milani <76585274+fede89-sw@users.noreply.github.com>
2021-02-23 23:16:50 +0100

    Add files via upload

--- File_BackUp.py
+++ File_BackUp.py
@@ -1,6 +1,16 @@
 import os
 import shutil
 import sys
+"""
+Scrivi una funzione "file_backup" che sia in grado 
+di effettuare copie di backup di determinati tipi di file, con le seguenti caratteristiche:
+
+- Percorso da scansionare, di backup e tipologia di file da copiare dovranno essere passati dall'utente 
+  tramite input
+- Il programma dovrà verificare la presenza o meno di una cartella di backup al percorso fornito
+  e qualora questa non fosse presente dovrà crearla
+- La funzione dovrà anche gestire l'eventuale scelta da parte dell'utente, di un percorso da scansionare 
+  che non esiste    """
 
 
 class BackUp:
@@ -10,15 +20,28 @@ class BackUp:
     self.scan = ""
     self.backup = ""
 
+  def create_tree(self, cartella):
+    """
+    ottengo le sottocartelle del percorso di scansione sottoforma di path
+    """
+    cartella_list = cartella.split("\\")
+    scan_list = self.scan.split("\\")
+    new_list = ""
+    for el in cartella_list:
+      if el not in scan_list:
+        new_list += ("\\" + el)
+    return new_list
+    
   def copy(self):    
     try:
       for cartella, _, files in os.walk(self.scan):
+        os.makedirs(self.backup + self.create_tree(cartella), exist_ok=True)
         for file in files:
           if self.scelta != "all":
             if file.endswith(self.scelta):
-              shutil.copy(os.path.join(cartella,file), self.backup)
+              shutil.copy(os.path.join(cartella,file), self.backup+self.create_tree(cartella))
           else:
-            shutil.copy(os.path.join(cartella,file), self.backup)
+            shutil.copy(os.path.join(cartella,file), self.backup+self.create_tree(cartella))
     except shutil.SameFileError as e:
       print(f"Errore nella copia dei file...{e}!\nUno o più file del percorso di scansione sono già presenti nella cartella di BackUp!\nRiprova selezionando 2 cartelle distinte!(Se i file sono nel Desktop,assicurati di metterli prima in una cartella!)")
     except:
